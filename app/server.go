0018b10a15bfd7385e64d2d6a0aa019c7e7eb520
Sirawit Chanaburanasak <97283645+punpunkeshin05@users.noreply.github.com>
2024-05-06 16:27:24 +0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -9,24 +9,46 @@ import (
 
 func main() {
 
+	// Listen on all interfaces on port 6379
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port 6379:", err)
 		os.Exit(1)
 	}
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer listener.Close()
+	fmt.Println("Server listening on port 6379")
+
+	// Accept connections
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		
+		// Handle net connection in a new goroutine
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("Connection from ", conn.RemoteAddr().String())
 
 	for {
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		// Read data from the connection
+		buffer := make([]byte, 1024)
+		n, err := conn.Read(buffer)
 		if err != nil {
-			fmt.Println("Error reading:", err.Error())
+			fmt.Println("Failed to read data: ", err.Error())
 			break
 		}
-		conn.Write([]byte("+PONG\r\n"))
+		
+		fmt.Println("Received: ", string(buffer[:n]))
+
+		// Write response to the connection
+		response := "+PONG\r\n"
+		conn.Write([]byte(response))
+		
 	}
 }
