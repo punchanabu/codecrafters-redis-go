77153d63f5037c331217bfac46818ec7f078e1ca
Sirawit Chanaburansak <punpunsirawit@gmail.com>
2024-05-09 12:42:11 +0000

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -15,8 +15,20 @@ func main() {
 
 	// Configure listening to port
 	var portFlag string
-	flag.StringVar(&portFlag, "port", "6379", "The port in which you wish to bind the redis service to")
+	var replicaOf string // To hold the replica of flag data if any
+
+	flag.StringVar(&portFlag, "port", "6379", "The port on which the Redis service will bind")
+	flag.StringVar(&replicaOf, "replicaof", "", "Specify master host and port if this instance is a replica")
 	flag.Parse()
+
+	// Determine role based on the replicaOf flag
+	role := "master"
+	if replicaOf != "" {
+		role = "slave"
+	}
+
+	// Initialize the Replica Config with the determined role
+	replicaConfig := config.NewReplicaConfig(role)
 
 	// Listen on all interfaces on port 6379
 	listener, err := net.Listen("tcp", "0.0.0.0:"+portFlag)
@@ -26,10 +38,6 @@ func main() {
 	}
 	defer listener.Close()
 	fmt.Println("Server listening on port 6379")
-
-	// Initialize the Replica Config
-	replicaConfig := config.NewReplicaConfig("master")
-
 
 	// Initialize the Store for Get and Set commands
 	redisStore := store.New()
